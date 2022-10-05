892a9c8b03492047e580cf9a1fdf202d7dccbe29
bilrdi <billiumardaeli@gmail.com>
2022-10-05 23:53:34 +0700

    Initial Commit

---
+++ Lawencon Test/Lawencon_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lawencon_TestApp.swift
+//  Lawencon Test
+//
+//  Created by Billi Umar Daeli on 05/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Lawencon_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
